4d1ac208c74692c3e2313cdf50c069aad0b553ad
dangquang <quangtd29@fpt.com.vn>
2024-10-05 15:45:41 +0700

    :tada: Init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    letterSpacing: {
+      tight: "-.015rem",
+    },
+    extend: { height: { "hafl-screen": "50vh" } },
+  },
+  plugins: [],
+};
